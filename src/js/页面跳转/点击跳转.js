c59c581cafb68b8d1cfba67f1960ab144c673b4a
PeiChien <925846753@qq.com>
2019-03-25 19:58:07 +0800

    Create 点击跳转.js

---
+++ src/js/页面跳转/点击跳转.js
@@ -0,0 +1,29 @@
+//import $ from 'jquery';
+
+$(document).ready(function () {
+
+    let fulla = $("#Home").offset().top;
+    let fullb = $("#About").offset().top;
+    let fullc = $("#Contact").offset().top;
+
+    function scr(index) {
+        $(".nav>li").removeClass("active").eq(index).addClass("active")
+    }
+
+    $(window).scroll(function () {
+        let scrTop = $(window).scrollTop() + fulla ;
+        if (scrTop >= fulla && scrTop < fullb) {
+            scr(0);
+        } else if (scrTop > fullb && scrTop < fullc) {
+            scr(1);
+        } else {
+            scr(2);
+        }
+    })
+
+    $("a[href^='#']").click(function () {
+        $("html, body").animate({
+            scrollTop: $(this.hash).offset().top
+        }, 700);
+    });
+});

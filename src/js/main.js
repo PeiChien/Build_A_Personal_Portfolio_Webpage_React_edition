2fd49fd9dd4e67f6f01917ec4e9b3ae0c330cc19
PeiChien <925846753@qq.com>
2019-03-25 19:57:40 +0800

    Create main.js

---
+++ src/js/main.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//导入第三方样式表
+//import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'font-awesome/css/font-awesome.min.css';
+
+//导入各部分模块
+import NavBar from '@/components/NavBar';
+import MainParts from '@/components/MainParts';
+import MyInfo from '@/components/MyInfo';
+import Footer from '@/components/Footer';
+
+//导入js动画文件
+import '@/页面跳转/点击跳转.js';
+import '@/贪吃蛇/难度选择';
+
+ReactDOM.render(
+<div>
+<NavBar></NavBar>
+<MainParts></MainParts>
+<MyInfo></MyInfo>
+<Footer></Footer>
+</div>,document.getElementById('app'))
+
+

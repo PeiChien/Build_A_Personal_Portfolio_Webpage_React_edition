714c2feed7c50ee63ed5b018e3b7a4da322b96da
PeiChien <925846753@qq.com>
2019-03-27 16:15:30 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
             exclude: /node_modules/,
             loader: 'babel-loader',
             options: {
-                presets: ['env', 'react']
+                presets: ['env','react']
             }
         },
         {test:/\.css$/,use:[MiniCssExtractPlugin.loader,'css-loader']},//modules启用模块化

bd474f3ad637f62cdb5d6dc2af1705df2f9e954d
PeiChien <925846753@qq.com>
2019-03-27 16:52:31 +0800

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 //导入第三方样式表
-//import 'bootstrap/dist/css/bootstrap.min.css';
-//import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 
 //导入各部分模块
 import {NavBar} from '@/components/NavBar';
